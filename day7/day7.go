0b8526a087bef75ef71ea2aec81548e09bcdaa2e
Tim Coninx <tim.coninx@gmail.com>
2022-12-07 11:43:21 +0100

    day7 - cleanup

--- day7/day7.go
+++ day7/day7.go
@@ -7,12 +7,6 @@ import (
 	"strconv"
 )
 
-const (
-	LISTING = "LISTING"
-	COMMAND = "COMMAND"
-	ROOT    = "/"
-)
-
 type dir struct {
 	name   string
 	files  map[string]int
@@ -26,57 +20,35 @@ func main() {
 	file, _ := os.Open("input.txt")
 	defer file.Close()
 	reader := bufio.NewScanner(file)
-	var line int
-	var status string
 	var root *dir = &dir{name: "/", files: make(map[string]int), dirs: make(map[string]*dir)}
 	root.parent = root
 	var currentdir *dir = root
 	for reader.Scan() {
-		line++
 		text := reader.Text()
 		matchcommand := regexp.MustCompile(`^\$`)
 		matchfile := regexp.MustCompile(`^\d+`)
 		if matchcommand.MatchString(text) {
-			// command mode is valid after every line
 			command := regexp.MustCompile(`^\$\s(\w+)\s?(.*)$`).FindStringSubmatch(text)
 			if command[1] == "cd" {
-				if command[2] == ROOT {
+				if command[2] == "/" {
 					currentdir = root
 				} else if command[2] == ".." {
-					if currentdir != root {
-						currentdir = currentdir.parent
-					}
+					currentdir = currentdir.parent
 				} else {
-					// assumption: all cd commands are correct
 					currentdir = currentdir.dirs[command[2]]
 				}
-				status = COMMAND
-			} else if command[1] == "ls" {
-				status = LISTING
-			} else {
-				panic("unknown command in text '" + text + "' (line " + strconv.Itoa(line) + ")")
 			}
 		} else {
-			// list mode is only valid after "ls" command has been given
-			if status != LISTING {
-				panic("line '" + text + "' came unexpected (at line " + strconv.Itoa(line) + ")")
-			}
 			if matchfile.MatchString(text) {
 				file := regexp.MustCompile(`^(\d+)\s(.+)$`).FindStringSubmatch(text)
-				//println("file " + file[2] + " in wd " + wd + " has size " + file[1])
 				filesize, _ := strconv.Atoi(file[1])
 				currentdir.files[file[2]] = filesize
 			} else {
 				dirname := regexp.MustCompile(`^dir\s(.*)$`).FindStringSubmatch(text)[1]
-				if currentdir.dirs[dirname] == nil {
-					currentdir.dirs[dirname] = &dir{name: dirname, files: make(map[string]int), dirs: make(map[string]*dir), parent: currentdir}
-				} else {
-					panic("twice the same dir listing in " + currentdir.name + " (dir : " + dirname + " )")
-				}
+				currentdir.dirs[dirname] = &dir{name: dirname, files: make(map[string]int), dirs: make(map[string]*dir), parent: currentdir}
 			}
 		}
 	}
-	// reduce
 	var rootsize int
 	for _, s := range root.files {
 		rootsize += s
